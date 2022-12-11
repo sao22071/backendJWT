ae3c31e20ebbf6037e63ba19868d5dc3913c41d7
santiago alvarez <santiago.alvarez@este.iudigital.edu.co>
2022-12-10 19:36:38 -0500

    git ignore

--- inventarios-frontend/src/helpers/axios-config.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  // baseURL: "http://localhost:4000/",
-  baseURL: "https://inventarios-back-iud.herokuapp.com",
-});
-
-export { axiosInstance };
--- inventarios-frontend/src/helpers/dateTime.js
+++
@@ -1,7 +0,0 @@
-export const funcionFecha = (fechaTimeStamp) => {
-  return new Date(new Date(fechaTimeStamp)).toLocaleDateString("es-CO");
-};
-
-export const funcionHora = (horafechaTimeStamp) => {
-  return new Date(new Date(horafechaTimeStamp)).toLocaleTimeString("es-CO");
-};
